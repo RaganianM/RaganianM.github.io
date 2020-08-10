46a517205faf8cc8e2e284998509e85ab66fe7a0
Margarita Raganian <m.raganyan@okenit.ru>
2020-08-10 15:18:45 +0300

    Добавлен todo

---
+++ todo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Todo />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Todo from './components/Todo'
+
+export default {
+  name: 'App',
+
+  components: {
+    Todo
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
